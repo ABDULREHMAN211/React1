6bb4a87733e4f144e4155b885bef6fe78d916023
ABDULREHMAN211 <bajwaarehmanbajwa@gmail.com>
2020-12-26 23:22:20 +0500

    1st basic react class

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import HELLO from'./HELLO';
+function App({name,age}){
+  return <div>Our Creator is great{name} AGE:{age-10}
+  <br/>
+  <HELLO firstName={name}></HELLO>
+</div>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
 }
+
 
 export default App;
---
+++ src/HELLO.js
@@ -0,0 +1,7 @@
+import React from'react';
+import './HELLO.css'
+
+function HELLO({firstName}){
+    return <p className="myName">This para is in Hello tag:{firstName}</p>
+}
+export default HELLO;
